38be9cb559f667d38d47e7b2e8b4f29510b76e3b
Doug Hoke <douglass.hoke@gmail.com>
2019-07-17 14:50:29 -0400

    Update build.bat
    output for debugging

--- build.bat
+++ build.bat
@@ -3,9 +3,15 @@
 nuget restore -Verbosity normal
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+echo "...build commencing..."
 msbuild -p:Configuration=Release ^
         -p:WarningLevel=0
 if %errorlevel% neq 0 exit /b %errorlevel%
+echo "...build ended..."
 
+pwd
+
+echo "...test commencing..."
 dotnet.exe test DatDotNet.csproj --configuration Release --no-build -v n
 if %errorlevel% neq 0 exit /b %errorlevel%
+echo "...test ended..."
